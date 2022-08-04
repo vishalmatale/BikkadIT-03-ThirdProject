a61542194326b0349006273a30a0843374b9c01a
vishalmatale <you@example.com>
2022-08-04 15:51:23 +0530

    Third project add to git repositery

---
+++ GitThirdProject/src/main/java/com/tcs/GitThirdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitThirdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitThirdProjectApplication.class, args);
+	}
+
+}
