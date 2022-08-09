94370c5c87b721936e9f3f48201603e22960dea8
vishalmatale <you@example.com>
2022-08-09 12:56:59 +0530

    Dao layer method

---
+++ GitThirdProject/src/main/java/com/tcs/Dao/DaoIMPI.java
@@ -0,0 +1,19 @@
+package com.tcs.Dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DaoIMPI {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Dao layer method started");
+		
+		
+		
+	}
+	
+	
+}
